0d51c5e58966e19e7b2aa7157aa10a08440728c0
mhirangz <almira.legaspina@gmail.com>
2022-02-27 02:29:00 +0800

    signin

--- project/urls.py
+++ project/urls.py
@@ -7,10 +7,14 @@ app_name = 'project'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name="home"),
+
     path('signin/', views.signin, name="signin"),
     path('signup/', views.signup, name="signup"),
+    path('logout',views.logOutUser,name ="logout_view"),
+
     path('protocol/',views.protocol),
-    path('lobby/',views.lobby),
+    path('lobby/',views.lobby,name="lobby"),
     path('rooms/',views.rooms),
+
     path('profile/',views.profile),
 ]
--- project/views.py
+++ project/views.py
@@ -2,8 +2,11 @@ from django.http import Http404
 from django.shortcuts import render, redirect
 from django.views.generic import View
 from django.http import HttpResponse
+from matplotlib.style import context
 from .forms import *
 from .models import *
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 
 def index(request):
@@ -16,12 +19,36 @@ def signup(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request,'Account was created')
+            return render(request,'signin.html',{'articles':'signin'})
+        else:
+            messages.warning(request,'Username or Password is not valid!')
+    context = {
+        'form':form
+    }
+    return render(request,'signup.html',context)
 
-    context = {'form':form}
-    return render(request, 'signup.html', context)
+  #  context = {'form':form}
+   # return render(request, 'signup.html', context)
 
 def signin(request):
+    if request.method =='POST':
+        username = request.POST.get('username')
+        password1 = request.POST.get('password')
+
+        user = authenticate(username=username, password = password1)
+        if user is not None:
+            login(request,user)
+            return render(request, 'userlobby.html',{'article':'lobby'})
+        else:
+            messages.info(request, 'Username or Password is incorrect!')
+
     return render(request, 'signin.html')
+
+def logOutUser(request):
+	logout(request)
+	return redirect('articles:sigin')
+
 
 def protocol(request):
     return render(request, 'protocol.html')
