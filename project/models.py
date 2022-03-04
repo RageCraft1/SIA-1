e1832a34b32aa4ff34d7759e5a3f62fcbcc2569a
hannahmhaesuarezlaquinon <93897271+hannahmhaesuarezlaquinon@users.noreply.github.com>
2022-03-04 12:29:37 +0800

    ok na ang date og time gilahi nako ang models.py

--- project/models.py
+++ project/models.py
@@ -5,7 +5,7 @@ class Reservation(models.Model):
     resId = models.AutoField(primary_key = True)
     availRooms = models.CharField(max_length=50, null=True)
     title = models.CharField(max_length=50, null=True)
-    resDate = models.DateField(null=True)
-    resTime = models.TimeField(null=True)
+    resDate = models.CharField(max_length=50, null=True)
+    resTime = models.CharField(max_length=50, null=True)
     class meta:
         db_table='Reservation'
