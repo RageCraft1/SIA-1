11c8c88e0f80567395e8eab2f6058de623d0ef46
hannahmhaesuarezlaquinon <93897271+hannahmhaesuarezlaquinon@users.noreply.github.com>
2022-03-23 21:43:00 +0800

    .

--- project/views.py
+++ project/views.py
@@ -129,7 +129,11 @@ class lobbyView(View):
                 print('recorded deleted')
 
         return redirect('lobbyView')
+<<<<<<< Updated upstream
         
+=======
+
+>>>>>>> Stashed changes
 #Brings user to the protocol submit page
 def protocol(request):
     return render(request, 'protocol.html')
