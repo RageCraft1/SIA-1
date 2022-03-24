f542b1c90f6d2042f470f498400263bdd2e46629
mhirangz <almira.legaspina@gmail.com>
2022-03-24 16:33:09 +0800

    kuha balik whitenoise

--- project/settings.py
+++ project/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
+   
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -142,7 +142,6 @@ STATICFILES_DIRS =(
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 MEDIA_URL= '/images/'
 
