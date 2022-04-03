2b30e3606e97b53af6156072a39217a0b6970407
mhirangz <almira.legaspina@gmail.com>
2022-04-03 20:13:29 +0800

    one more

--- project/settings.py
+++ project/settings.py
@@ -134,22 +134,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS =(
     os.path.join(BASE_DIR,'static'),
 )
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-
-
 MEDIA_URL= '/images/'
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
-#MEDIA_ROOT= os.path.join(BASE_DIR, 'static/images')
-
-#MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 # Path where media is stored
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
