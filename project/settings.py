82f19b12e2f419c1d17b4782cf9ae2769159e4bf
mhirangz <almira.legaspina@gmail.com>
2022-04-05 00:37:12 +0800

    never give up

--- project/settings.py
+++ project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6j)ha!wet@^n=(*8*77s0ikkd2wvf@$&0=-9e^w2g17_pu=i7z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['group2-coreconferenceroomreservationsystem.azurewebsites.net']
 
 
@@ -135,16 +135,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-#STATICFILES_DIRS =(
-#    os.path.join(BASE_DIR,'static'),
-#)
+STATICFILES_DIRS =(
+   os.path.join(BASE_DIR,'static'),
+)
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL= '/images/'
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Path where media is stored
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
